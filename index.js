8a22662aaea35403f2c6acf6e4b6c5bb7543a738
Chensienyong <ashxcellent_kingzr@yahoo.com>
2017-11-17 12:09:19 +0700

    errrr

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ Promise.config({
     cancellation: true
   });
 const TelegramBot = require('node-telegram-bot-api');
-const token = '461308497:AAEN6IgZUOPvBrnD11zEjTv6QxpgjkT6zMI';
+const TOKEN = '461308497:AAEN6IgZUOPvBrnD11zEjTv6QxpgjkT6zMI';
+const url = 'https://aura-kasih-bot.herokuapp.com/';
+const port = 8000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(TOKEN, {polling: true});
 const group_id = -296076031
 //https://api.telegram.org/bot{token}/{method}
 //data={'chat_id': 12345, 'text': 'hello friend'}
