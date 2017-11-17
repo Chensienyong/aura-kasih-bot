99c3580e4a45bcf09c83b6e2b237003911123810
Chensienyong <ashxcellent_kingzr@yahoo.com>
2017-11-18 00:01:40 +0700

    a

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const Promise = require('bluebird');
 Promise.config({
     cancellation: true
   });
-const TelegramBot = require('node-telegram-bot-api');
+
+  const TelegramBot = require('node-telegram-bot-api');
 const TOKEN = '461308497:AAEN6IgZUOPvBrnD11zEjTv6QxpgjkT6zMI';
 // const url = 'https://aura-kasih-bot.herokuapp.com/';
 const url = 'http://local.host:8000';
 const port = process.env.PORT || 8000;
-
-const express = require('express');
-const bodyParser = require('body-parser');
 const bot = new TelegramBot(TOKEN, {polling: true});
 const group_id = -296076031
+
 //https://api.telegram.org/bot{token}/{method}
 //data={'chat_id': 12345, 'text': 'hello friend'}
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post(`/bot${TOKEN}`, (req, res) => {
-    /* res.sendStatus(200); */
-    res.send(JSON.stringify(req));
+    console.log('body is ',req.body);
+    res.sendStatus(200);
 });
 
 app.get('/', function(req, res){
