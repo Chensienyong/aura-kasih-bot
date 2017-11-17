05506907bca18ba0e4a65e6060a638c6391916f8
Chensienyong <ashxcellent_kingzr@yahoo.com>
2017-11-17 11:02:15 +0700

    er

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ Promise.config({
   });
 const TelegramBot = require('node-telegram-bot-api');
 const token = '461308497:AAEN6IgZUOPvBrnD11zEjTv6QxpgjkT6zMI';
-const bot = new TelegramBot(token);
+const bot = new TelegramBot(token, {polling: true});
 const group_id = -296076031
 //https://api.telegram.org/bot{token}/{method}
 //data={'chat_id': 12345, 'text': 'hello friend'}
